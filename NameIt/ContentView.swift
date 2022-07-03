a67093586051be023229e7ac3ba2bd041c32edfe
Fiona Morgan <fkmorgan@me.com>
2022-07-03 18:01:09 +0100

    Basics built... now to add Firestore

--- NameIt/ContentView.swift
+++ NameIt/ContentView.swift
@@ -8,69 +8,145 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+enum FocusableField: Hashable {
+    case word
+}
 
+enum InputType {
+    case text
+    case numerical
+}
+
+struct List: Identifiable {
+    var id: String = UUID().uuidString
+    var title: String
+    var question: String
+    var words: [String]
+    var timer: Int
+    var inputType: InputType
+}
+
+struct ContentView: View {
+    
+    @State var timeRemaining = 0
+        let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
+    var list: List = List(id: "001", title: "Colours of the Rainbow", question: "Name all the colours of the rainbow", words: ["blue", "green", "orange", "red", "yellow", "indigo", "violet"], timer: 60, inputType: .text)
+    @State private var word: String = ""
+    @FocusState private var focus: FocusableField?
+    @State var gameStarted: Bool = false
+    @State var gameEnded: Bool = false
+    private var answerArray: [String]
+    @State var correctAnswersGiven: [String] = []
+    
+    init() {
+        answerArray = list.words
+    }
+    
     var body: some View {
+        
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            if gameStarted {
+                VStack{
+                    Spacer()
+                    Text("\(timeRemaining)")
+                                .onReceive(timer) { _ in
+                                    if timeRemaining > 0 {
+                                        timeRemaining -= 1
+                                    }
+                                }
+                    Text(list.question)
+                    Spacer()
+                    HStack{
+                        Spacer()
+                        TextField("Type your answer", text: $word)
+                            .disableAutocorrection(true)
+                            .textCase(.lowercase)
+                            .textInputAutocapitalization(.never)
+                            .padding(.horizontal)
+                            .textFieldStyle(.roundedBorder)
+                            .focused($focus, equals: .word)
+                        Button {
+                            clearWord()
+                        } label: {
+                            Image(systemName: "multiply.circle.fill")
+                        }
+                        Spacer()
+                    }
+                    Spacer()
+                    Button("Give up") {
+                        endGame()
+                    }
+                    Text(correctAnswersGiven.joined(separator:", " ))
+                    Spacer()
+                }
+                .onChange(of: word) { newValue in
+                    checkWord()
+                }
+                .onChange(of: timeRemaining) { newValue in
+                    timerCheck()
+                }
+                
+            } else if gameEnded {
+                VStack {
+                if answerArray.count == correctAnswersGiven.count {
+                    Text("well done getting them all")
+                } else if timeRemaining == 0 {
+                    Text("Time ran out")
+                } else {
+                    Text("You gave up? :(")
+                }
+                    Button("Start new game") {
+                        startGame()
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                
+                
+            } else {
+                Button("Start game") {
+                    startGame()
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    private func checkWord() {
+        if answerArray.contains(word.lowercased()) && !correctAnswersGiven.contains(word.lowercased()){
+            correctAnswersGiven.append(word.lowercased())
+            clearWord()
+        } else if answerArray.count == correctAnswersGiven.count {
+            endGame()
         }
+    }
+    
+    private func clearWord() {
+        word = ""
+    }
+    
+    private func startGame() {
+        correctAnswersGiven = []
+        word = ""
+        gameStarted = true
+        timeRemaining = list.timer
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+            focus = .word
+        }
+    }
+    
+    private func timerCheck() {
+        if timeRemaining == 0 {
+            endGame()
+        }
+    }
+    
+    private func endGame() {
+        gameStarted = false
+        gameEnded = true
     }
 }
 
